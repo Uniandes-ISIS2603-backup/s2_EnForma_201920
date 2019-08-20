8663d883a7a6ca3ee7f43dd28263ce3999aee4cd
Jimmy Pepinosa <jd.pepinosa@uniandes.edu.co>
2019-08-20 10:58:00 -0500

    Persistence, test, entity de la clase Calificacion

---
+++ s2_enforma-back/src/main/java/co/edu/uniandes/csw/enforma/persistence/CalificacionPersistence.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.enforma.persistence;
+
+import co.edu.uniandes.csw.enforma.entities.CalificacionEntity;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Jimmy Pepinosa
+ */
+@Stateless
+public class CalificacionPersistence 
+{
+    public CalificacionEntity create(CalificacionEntity estudiante)
+    {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+}
