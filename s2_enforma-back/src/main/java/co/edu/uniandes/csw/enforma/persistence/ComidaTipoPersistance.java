d300efba4e0b4aa21adb499d914bb179c46db841
jose manuel florez <jm.florezc@uniandes.edu.co>
2019-08-20 11:01:21 -0500

    Tabla de la clase ComidaTipo - intento 4

---
+++ s2_enforma-back/src/main/java/co/edu/uniandes/csw/enforma/persistence/ComidaTipoPersistance.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.enforma.persistence;
+
+import co.edu.uniandes.csw.enforma.entities.ComidaTipo;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Estudiante
+ */
+
+@Stateless
+
+public class ComidaTipoPersistance {
+    
+    
+    
+    public ComidaTipo create(ComidaTipo comidaTipo)
+    {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
