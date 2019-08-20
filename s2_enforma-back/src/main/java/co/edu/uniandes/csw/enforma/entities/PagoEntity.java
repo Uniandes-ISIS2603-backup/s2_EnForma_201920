5e859828c0cb4dc304700ac4e44d3217826a4bde
ev.jaimes@uniandes.edu.co <>
2019-08-20 10:25:13 -0500

    PagoEntity raw

---
+++ s2_enforma-back/src/main/java/co/edu/uniandes/csw/enforma/entities/PagoEntity.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.csw.enforma.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Elina Jaimes
+ */
+@Entity
+public class PagoEntity extends BaseEntity implements Serializable
+{
+    
+    
+    private Double monto;
+    private Integer numeroTarjeta;
+
+    /**
+     * @return the monto
+     */
+    public Double getMonto() {
+        return monto;
+    }
+
+    /**
+     * @param monto the monto to set
+     */
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    /**
+     * @return the numeroTarjeta
+     */
+    public Integer getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    /**
+     * @param numeroTarjeta the numeroTarjeta to set
+     */
+    public void setNumeroTarjeta(Integer numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+    
+}
