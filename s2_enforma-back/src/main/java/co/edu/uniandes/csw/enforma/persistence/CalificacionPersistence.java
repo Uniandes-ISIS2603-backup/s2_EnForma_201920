ed9eb78020a8b244777075a69ea0f5653859ad61
Jimmy Pepinosa <jd.pepinosa@uniandes.edu.co>
2019-08-28 20:29:46 -0500

    Calificacion persistencia y test acabados y en verde

--- s2_enforma-back/src/main/java/co/edu/uniandes/csw/enforma/persistence/CalificacionPersistence.java
+++ s2_enforma-back/src/main/java/co/edu/uniandes/csw/enforma/persistence/CalificacionPersistence.java
@@ -6,9 +6,11 @@
 package co.edu.uniandes.csw.enforma.persistence;
 
 import co.edu.uniandes.csw.enforma.entities.CalificacionEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -20,10 +22,70 @@ public class CalificacionPersistence
     @PersistenceContext(unitName = "enformaPU")
     protected EntityManager em;
     
+    /**
+     * Crea un autor en la base de datos
+     * @param calificacion objeto calificacion que se creara en la base de datos
+     * @return retorna la calificacion creada con un id dado por la base de datos
+     */
     public CalificacionEntity create(CalificacionEntity calificacion)
     {
-        //throw new java.lang.UnsupportedOperationException("Not supported yet.");
         em.persist(calificacion);
         return calificacion;
     }
+    
+    /**
+     * Devuelve todas las calificaciones de la base de datos
+     * @return una lista con todas las calificaciones que encuentre en la base datos, 
+     * "select u from AuthorEntity u" es como un "select * from AuthorEntity;" - "SELECT * FROM table_name" en SQL.
+     */
+    public List<CalificacionEntity> findAll()
+    {
+        // Se crea un query para buscar todas las calificaciones en la base de datos.
+        TypedQuery query = em.createQuery("select u from CalificacionEntity u", CalificacionEntity.class);
+        // Note que en el query se hace uso del método getResultList() que obtiene una lista de calificaciones.
+        return query.getResultList();
+    }
+    
+    /**
+     * Busca si hay alguna calificacion con el id que se eniva
+     * @param calificacionId id correspondiente a la calificacion buscada
+     * @return la calificacion buscada
+     */
+    public CalificacionEntity find(Long calificacionId)
+    {
+        /* Note que se hace uso del metodo "find" propio del EntityManager, el cual recibe como argumento 
+         *el tipo de la clase y el objeto que nos hara el filtro en la base de datos en este caso el "id"
+         *Suponga que es algo similar a "select * from CalificacionEntity where id=id;" - "SELECT * FROM table_name WHERE condition;" en SQL.
+         */
+        return em.find(CalificacionEntity.class, calificacionId);
+    }
+    
+    /**
+     * Actualiza a la calificacion dada
+     * @param calificacionEntity la calificacion que viene con el o los nuevos cambios
+     * @return una calificacion con los cambios aplicados
+     */
+    public CalificacionEntity update(CalificacionEntity calificacionEntity)
+    {
+         /* Note que hacemos uso de un método propio del EntityManager llamado merge() que recibe como argumento
+          *la calificacion con los cambios, esto es similar a 
+          *"UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
+          */
+        return em.merge(calificacionEntity);
+    }
+    
+    /**
+     * Borra la calificacion dada por el id de la base de datos
+     * @param calificacionId id correspondiente de la calificacion a borrar
+     */
+    public void delete(Long calificacionId)
+    {
+        // Se hace uso de mismo método que esta explicado en public CalificacionEntity find(Long id) para obtener la acalificacion a borrar.
+        CalificacionEntity calificacionEntity = em.find(CalificacionEntity.class, calificacionId);
+        /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
+        EntityManager para eliminar de la base de datos el objeto que encontramos y queremos borrar.
+        Es similar a "delete from  CalificacionEntity where id=id;" - "DELETE FROM table_name WHERE condition;" en SQL.*/
+        em.remove(calificacionEntity);
+    }
 }
+
--- s2_enforma-back/src/test/java/co/edu/uniandes/csw/enforma/test/persistence/CalificacionPersistenceTest.java
+++ s2_enforma-back/src/test/java/co/edu/uniandes/csw/enforma/test/persistence/CalificacionPersistenceTest.java
@@ -7,7 +7,12 @@ package co.edu.uniandes.csw.enforma.test.persistence;
 
 import co.edu.uniandes.csw.enforma.entities.CalificacionEntity;
 import co.edu.uniandes.csw.enforma.persistence.CalificacionPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -15,6 +20,7 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.Assert;
+import org.junit.Before;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -23,22 +29,79 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  * @author Jimmy Pepinosa
  */
 @RunWith(Arquillian.class)
-public class CalificacionPersistenceTest extends CalificacionPersistence
-{
+public class CalificacionPersistenceTest 
+{ 
+    @Inject
+    CalificacionPersistence cp;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    private List<CalificacionEntity> data = new ArrayList<>();
+    
+    
     @Deployment
     public static JavaArchive createDeployment()
     {
         return ShrinkWrap.create(JavaArchive.class)
                 .addClass(CalificacionEntity.class)
                 .addClass(CalificacionPersistence.class)
+                .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml","beans.xml");
     }
     
-    @Inject
-    CalificacionPersistence cp;
+     /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() 
+    {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
     
+     /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() 
+    {
+        em.createQuery("delete from CalificacionEntity").executeUpdate();
+    }
+    
+     /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las pruebas.
+     */
+    private void insertData() 
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) 
+        {
+            CalificacionEntity entity = factory.manufacturePojo(CalificacionEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }    
+    
+    /**
+     * Prueba para crear una calificacion
+     */
     @Test
-    public void createTest()
+    public void createCalifiacionTest()
     {
         PodamFactory factory = new PodamFactoryImpl();
         CalificacionEntity calificacion = factory.manufacturePojo(CalificacionEntity.class);
@@ -50,4 +113,72 @@ public class CalificacionPersistenceTest extends CalificacionPersistence
         
         Assert.assertEquals(calificacion.getPuntaje(), entity.getPuntaje());
     }
+    
+    /**
+     * Prueba para consultar la lista de Calificacion
+     */
+    @Test
+    public void getCalificacionesTest()
+    {
+        List<CalificacionEntity> list = cp.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for(CalificacionEntity ent : list)
+        {
+            boolean  encontrada = false;
+            for(CalificacionEntity entity : data)
+            {
+                if(ent.getId().equals(entity.getId()))
+                {
+                    encontrada = true;
+                }
+            }
+            Assert.assertTrue(encontrada);
+        }
+    }
+    
+    /**
+     * Prueba para consultar una calificacion
+     */
+    public void getCalificacionTest()
+    {
+        CalificacionEntity entity = data.get(0);
+        CalificacionEntity newEntity = cp.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getId(), newEntity.getId());
+        Assert.assertEquals(entity.getPuntaje(), newEntity.getPuntaje());
+        Assert.assertEquals(entity.getComentario(), newEntity.getComentario());
+    }
+    
+    /**
+     * Prueba para actualizar una calificacion
+     */
+    @Test
+    public void updateCalificacionTest()
+    {
+        CalificacionEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        CalificacionEntity newEntity = factory.manufacturePojo(CalificacionEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        cp.update(newEntity);
+
+        CalificacionEntity resp = em.find(CalificacionEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getId(), resp.getId());
+    }
+    
+    /**
+     * Prueba para eliminar una calificacion
+     */
+    @Test
+    public void deletelificacionTest()
+    {
+        CalificacionEntity entity = data.get(0);
+        cp.delete(entity.getId());
+        CalificacionEntity deleted = em.find(CalificacionEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+            
 }
