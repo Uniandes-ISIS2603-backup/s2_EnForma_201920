6e2d835105ed38cd6d2319f2e79e151353d8c392
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2019-09-10 10:49:54 -0500

    Administrador
    Entity, Persistence, Test

---
+++ s2_enforma-back/src/main/java/co/edu/uniandes/csw/enforma/persistence/AdministradorPersistence.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.enforma.persistence;
+import co.edu.uniandes.csw.enforma.entities.AdministradorEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Elina Jaimes
+ */
+
+@Stateless
+public class AdministradorPersistence {
+  
+     @PersistenceContext(unitName="enformaPU")
+    protected EntityManager em;
+    
+    
+    public AdministradorEntity create (AdministradorEntity admin)
+    {
+        em.persist(admin);
+        return admin;
+    }
+    
+    
+    public AdministradorEntity find(Long adminID)
+    {
+        return em.find(AdministradorEntity.class, adminID);
+    }
+    
+    
+    public List<AdministradorEntity> findAll()
+    {
+        // Se crea un query para buscar todas los admins en la base de datos.
+        TypedQuery<AdministradorEntity> query = em.createQuery("select u from AdministradorEntity u", AdministradorEntity.class);
+        // Note que en el query se hace uso del método getResultList() que obtiene una lista de admins.
+        return query.getResultList();
+    }
+    
+    public AdministradorEntity update(AdministradorEntity admin)
+    {
+        //Es equivalente a un comando de SQL que permite actualizar la info
+        return em.merge(admin);
+    }
+    
+    
+    public void delete(Long adminID)
+    {
+        AdministradorEntity d= em.find(AdministradorEntity.class, adminID);
+        em.remove(d);
+    }
+    
+    
+}
