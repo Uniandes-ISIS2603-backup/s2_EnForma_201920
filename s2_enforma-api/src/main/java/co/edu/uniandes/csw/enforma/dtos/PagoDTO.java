25da295b9ac32fe85edac4187e6bf26846bd064a
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2019-09-24 15:10:40 -0500

    AdministradorDTO, AdministradorDetail, PagoDTO
    Los DTOS

---
+++ s2_enforma-api/src/main/java/co/edu/uniandes/csw/enforma/dtos/PagoDTO.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.enforma.dtos;
+
+import co.edu.uniandes.csw.enforma.entities.PagoEntity;
+import java.io.Serializable;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ *
+ * @author Elina Jaimes
+ */
+public class PagoDTO implements Serializable{
+    
+    private Long id;
+    private Double monto;
+    private Integer numeroTarjeta;
+    private Boolean esPrepago;
+    private String estadoPago;
+    
+    public PagoDTO(){
+        
+    }
+        
+    public PagoDTO(PagoEntity pagoEntity)
+    {
+        if(pagoEntity!=null)
+        {
+            this.id=pagoEntity.getId();
+            this.monto=pagoEntity.getMonto();
+            this.esPrepago=pagoEntity.getEsPrepago();
+            this.estadoPago=pagoEntity.getEstadoPago();
+            this.numeroTarjeta=pagoEntity.getNumeroTarjeta();
+        }
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the monto
+     */
+    public Double getMonto() {
+        return monto;
+    }
+
+    /**
+     * @param monto the monto to set
+     */
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    /**
+     * @return the numeroTarjeta
+     */
+    public Integer getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    /**
+     * @param numeroTarjeta the numeroTarjeta to set
+     */
+    public void setNumeroTarjeta(Integer numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    /**
+     * @return the esPrepago
+     */
+    public Boolean getEsPrepago() {
+        return esPrepago;
+    }
+
+    /**
+     * @param esPrepago the esPrepago to set
+     */
+    public void setEsPrepago(Boolean esPrepago) {
+        this.esPrepago = esPrepago;
+    }
+
+    /**
+     * @return the estadoPago
+     */
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    /**
+     * @param estadoPago the estadoPago to set
+     */
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+    
+     @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+    
+}
