1b668b121b13548c5fdb0f4a206d30cde45de009
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2019-10-08 19:06:42 -0500

    Administrador Resource y DTO en wait

--- s2_enforma-api/src/main/java/co/edu/uniandes/csw/enforma/dtos/AdministradorDetailDTO.java
+++ s2_enforma-api/src/main/java/co/edu/uniandes/csw/enforma/dtos/AdministradorDetailDTO.java
@@ -6,7 +6,10 @@
 package co.edu.uniandes.csw.enforma.dtos;
 
 import co.edu.uniandes.csw.enforma.entities.AdministradorEntity;
+import co.edu.uniandes.csw.enforma.entities.DietaTipoEntity;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -14,45 +17,43 @@ import java.io.Serializable;
  */
 public class AdministradorDetailDTO extends AdministradorDTO implements Serializable {
 
-//// relación  uno o muchos reviews 
-//    private List<DietaTipoDTO> reviews;
-//
-//    // relación  cero o muchos author
-//    private List<ComidaTipoDTO> authors;
-//
-//    public AdministradorDetailDTO() {
-//        super();
-//    }
-//
-//    /**
-//     * Constructor para transformar un Entity a un DTO
-//     *
-//     * @param bookEntity La entidad de la cual se construye el DTO
-//     */
-//    public AdministradorDetailDTO(AdministradorEntity bookEntity) {
-//        super(bookEntity);
-//        if (bookEntity.getDietaTipos() != null) {
-//            reviews = new ArrayList<>();
+// relación  uno o muchos reviews 
+    private List<DietaTipoDTO> reviews;
+
+    // relación  cero o muchos author
+    private List<ComidaTipoDTO> authors;
+
+  
+
+    /**
+     * Constructor para transformar un Entity a un DTO
+     *
+     * @param bookEntity La entidad de la cual se construye el DTO
+     */
+    public AdministradorDetailDTO(AdministradorEntity bookEntity) {
+        super(bookEntity);
+        if (bookEntity != null) {
+            reviews = new ArrayList<>();
 //            for (DietaTipoEntity entityDietaTipo : bookEntity.getDietaTipos()) {
 //                reviews.add(new DietaTipoDTO(entityDietaTipo));
 //            }
-//        }
+        }
 //        if (bookEntity.getComidaTipos() != null) {
 //            authors = new ArrayList<>();
 //            for (ComidaTipoEntity entityComidaTipo : bookEntity.getComidaTipos()) {
 //                authors.add(new ComidaTipoDTO(entityComidaTipo));
 //            }
 //        }
-//    }
-//
-//    /**
-//     * Transformar el DTO a una entidad
-//     *
-//     * @return La entidad que representa el libro.
-//     */
-//    @Override
-//    public AdministradorEntity toEntity() {
-//        AdministradorEntity bookEntity = super.toEntity();
+    }
+
+    /**
+     * Transformar el DTO a una entidad
+     *
+     * @return La entidad que representa el libro.
+     */
+    @Override
+    public AdministradorEntity toEntity() {
+        AdministradorEntity bookEntity = super.toEntity();
 //        if (reviews != null) {
 //            List<DietaTipoEntity> reviewsEntity = new ArrayList<>();
 //            for (DietaTipoDTO dtoDietaTipo : getDietaTipos()) {
@@ -67,44 +68,44 @@ public class AdministradorDetailDTO extends AdministradorDTO implements Serializ
 //            }
 //            bookEntity.setComidaTipos(authorsEntity);
 //        }
-//        return bookEntity;
-//    }
-//
-//    /**
-//     * Devuelve las reseñas asociadas a este libro
-//     *
-//     * @return Lista de DTOs de Reseñas
-//     */
-//    public List<DietaTipoDTO> getDietaTipos() {
-//        return reviews;
-//    }
-//
-//    /**
-//     * Modifica las reseñas de este libro.
-//     *
-//     * @param reviews Las nuevas reseñas
-//     */
-//    public void setDietaTipos(List<DietaTipoDTO> reviews) {
-//        this.reviews = reviews;
-//    }
-//
-//    /**
-//     * Devuelve los autores del libro
-//     *
-//     * @return DTO de Autores
-//     */
-//    public List<ComidaTipoDTO> getComidaTipos() {
-//        return authors;
-//    }
-//
-//    /**
-//     * Modifica los autores del libro
-//     *
-//     * @param authors Lista de autores
-//     */
-//    public void setComidaTipos(List<ComidaTipoDTO> authors) {
-//        this.authors = authors;
-//    }
+        return bookEntity;
+    }
+
+    /**
+     * Devuelve las reseñas asociadas a este libro
+     *
+     * @return Lista de DTOs de Reseñas
+     */
+    public List<DietaTipoDTO> getDietaTipos() {
+        return reviews;
+    }
+
+    /**
+     * Modifica las reseñas de este libro.
+     *
+     * @param reviews Las nuevas reseñas
+     */
+    public void setDietaTipos(List<DietaTipoDTO> reviews) {
+        this.reviews = reviews;
+    }
+
+    /**
+     * Devuelve los autores del libro
+     *
+     * @return DTO de Autores
+     */
+    public List<ComidaTipoDTO> getComidaTipos() {
+        return authors;
+    }
+
+    /**
+     * Modifica los autores del libro
+     *
+     * @param authors Lista de autores
+     */
+    public void setComidaTipos(List<ComidaTipoDTO> authors) {
+        this.authors = authors;
+    }
 
     
 }
--- s2_enforma-api/src/main/java/co/edu/uniandes/csw/enforma/resources/AdministradorResource.java
+++ s2_enforma-api/src/main/java/co/edu/uniandes/csw/enforma/resources/AdministradorResource.java
@@ -6,24 +6,32 @@
 package co.edu.uniandes.csw.enforma.resources;
 
 import co.edu.uniandes.csw.enforma.dtos.AdministradorDTO;
+import co.edu.uniandes.csw.enforma.dtos.AdministradorDetailDTO;
 import co.edu.uniandes.csw.enforma.ejb.AdministradorLogic;
 import co.edu.uniandes.csw.enforma.entities.AdministradorEntity;
 import co.edu.uniandes.csw.enforma.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
- * @author Elina Jaimes
+ * @administrador Elina Jaimes
  */
 
-@Path("administradores")
+@Path("administradoristradores")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
@@ -35,29 +43,120 @@ public class AdministradorResource {
     private AdministradorLogic administradorLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
     
     /**
-     * Crea un nuevo administrador con la informacion que se recibe en el cuerpo de
+     * Crea un nuevo administradoristrador con la informacion que se recibe en el cuerpo de
      * la petición y se regresa un objeto identico con un id auto-generado por
      * la base de datos.
      *
-     * @param administrador {@link AdministradorDTO} - el administrador que se desea
+     * @param administradoristrador {@link AdministradorDTO} - el administradoristrador que se desea
      * guardar.
-     * @return JSON {@link AdministradorDTO} - el administrador guardada con el atributo
+     * @return JSON {@link AdministradorDTO} - el administradoristrador guardada con el atributo
      * id autogenerado.
      * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} -
-     * Error de lógica que se genera cuando ya existe el administrador.
+     * Error de lógica que se genera cuando ya existe el administradoristrador.
      */
     @POST
-    public AdministradorDTO createAdministrador(AdministradorDTO administrador) throws BusinessLogicException 
+    public AdministradorDTO createAdministrador(AdministradorDTO administradoristrador) throws BusinessLogicException 
     {
-        LOGGER.log(Level.INFO, "AdministradorResource createAdministrador: input: {0}", administrador);
+        LOGGER.log(Level.INFO, "AdministradorResource createAdministrador: input: {0}", administradoristrador);
         // Convierte el DTO (json) en un objeto Entity para ser manejado por la lógica.
-        AdministradorEntity administradorEntity = administrador.toEntity();
+        AdministradorEntity administradoristradorEntity = administradoristrador.toEntity();
         // Invoca la lógica para crear la editorial nueva
-        AdministradorEntity nuevoAdministradorEntity = administradorLogic.crearAdministrador(administradorEntity);
+        AdministradorEntity nuevoAdministradorEntity = administradorLogic.crearAdministrador(administradoristradorEntity);
         // Como debe retornar un DTO (json) se invoca el constructor del DTO con argumento el entity nuevo
         AdministradorDTO nuevoAdministradorDTO = new AdministradorDTO(nuevoAdministradorEntity);
         LOGGER.log(Level.INFO, "AdministradorResource createAdministrador: output: {0}", nuevoAdministradorDTO);
         return nuevoAdministradorDTO;
     }
     
+     /**
+     * Busca y devuelve todos los autores que existen en la aplicacion.
+     *
+     * @return JSONArray {@link AdministradorDetailDTO} - Los autores encontrados en la
+     * aplicación. Si no hay ninguno retorna una lista vacía.
+     */
+    @GET
+    public List<AdministradorDetailDTO> getAdministradors() {
+        LOGGER.info("AdministradorResource getAdministradors: input: void");
+        List<AdministradorDetailDTO> listaAdministradors = listEntity2DTO(administradorLogic.getAdministradors());
+        LOGGER.log(Level.INFO, "AdministradorResource getAdministradors: output: {0}", listaAdministradors);
+        return listaAdministradors;
+    }
+
+    /**
+     * Busca el autor con el id asociado recibido en la URL y lo devuelve.
+     *
+     * @param administradorsId Identificador del autor que se esta buscando. Este debe
+     * ser una cadena de dígitos.
+     * @return JSON {@link AdministradorDetailDTO} - El autor buscado
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra el autor.
+     */
+    @GET
+    @Path("{administradorsId: \\d+}")
+    public AdministradorDetailDTO getAdministrador(@PathParam("administradorsId") Long administradorsId) {
+        LOGGER.log(Level.INFO, "AdministradorResource getAdministrador: input: {0}", administradorsId);
+        AdministradorEntity administradorEntity = administradorLogic.getAdministrador(administradorsId);
+        if (administradorEntity == null) {
+            throw new WebApplicationException("El recurso /administradors/" + administradorsId + " no existe.", 404);
+        }
+        AdministradorDetailDTO detailDTO = new AdministradorDetailDTO(administradorEntity);
+        LOGGER.log(Level.INFO, "AdministradorResource getAdministrador: output: {0}", detailDTO);
+        return detailDTO;
+    }
+
+    /**
+     * Actualiza el autor con el id recibido en la URL con la información que se
+     * recibe en el cuerpo de la petición.
+     *
+     * @param administradorsId Identificador del autor que se desea actualizar. Este
+     * debe ser una cadena de dígitos.
+     * @param administrador {@link AdministradorDetailDTO} El autor que se desea guardar.
+     * @return JSON {@link AdministradorDetailDTO} - El autor guardado.
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra el autor a
+     * actualizar.
+     */
+    @PUT
+    @Path("{administradorsId: \\d+}")
+    public AdministradorDetailDTO updateAdministrador(@PathParam("administradorsId") Long administradorsId, AdministradorDetailDTO administrador) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "AdministradorResource updateAdministrador: input: administradorsId: {0} , administrador: {1}", new Object[]{administradorsId, administrador});
+        administrador.setId(administradorsId);
+        if (administradorLogic.getAdministrador(administradorsId) == null) {
+            throw new WebApplicationException("El recurso /administradors/" + administradorsId + " no existe.", 404);
+        }
+        AdministradorDetailDTO detailDTO = new AdministradorDetailDTO(administradorLogic.updateAdministrador(administradorsId, administrador.toEntity()));
+        LOGGER.log(Level.INFO, "AdministradorResource updateAdministrador: output: {0}", detailDTO);
+        return detailDTO;
+    }
+
+    /**
+     * Borra el autor con el id asociado recibido en la URL.
+     *
+     * @param administradorsId Identificador del autor que se desea borrar. Este debe
+     * ser una cadena de dígitos.
+     * @throws co.edu.uniandes.csw.bookstore.exceptions.BusinessLogicException
+     * si el autor tiene libros asociados
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper}
+     * Error de lógica que se genera cuando no se encuentra el autor a borrar.
+     */
+    @DELETE
+    @Path("{administradorsId: \\d+}")
+    public void deleteAdministrador(@PathParam("administradorsId") Long administradorsId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "AdministradorResource deleteAdministrador: input: {0}", administradorsId);
+        if (administradorLogic.getAdministrador(administradorsId) == null) {
+            throw new WebApplicationException("El recurso /administradors/" + administradorsId + " no existe.", 404);
+        }
+        administradorLogic.deleteAdministrador(administradorsId);
+        LOGGER.info("AdministradorResource deleteAdministrador: output: void");
+    }
+    
+    
+    private List<AdministradorDetailDTO> listEntity2DTO(List<AdministradorEntity> entityList) {
+        List<AdministradorDetailDTO> list = new ArrayList<>();
+        for (AdministradorEntity entity : entityList) {
+            list.add(new AdministradorDetailDTO(entity));
+        }
+        return list;
+    
+}
 }
